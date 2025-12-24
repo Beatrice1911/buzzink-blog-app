641b1adc4277e7a422a1cbd88fa85d39ce8f4fe2
Beatrice Okereke <beatriceokereke19@gmail.com>
2025-12-24 15:19:58 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,8 @@ app.use(session({
 
 const allowedOrigins = [
   "http://127.0.0.1:5500",
-  "http://localhost:5500"
+  "http://localhost:5500",
+  "https://buzzink.onrender.com"
 ];
 
 const corsOptions = {
