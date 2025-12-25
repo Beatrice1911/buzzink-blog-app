13ecc5c90a40d46fd3e4c8f557bc92be23fd1e5e
Beatrice1911 <beatriceokereke19@gmail.com>
2025-12-25 20:32:31 +0100

    Implemented the use of cloudinary

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../cloudinary");
 const path = require("path");
 const { requireAuth, optionalAuth } = require("../middleware/auth");
 
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, path.join(__dirname, "..", "uploads")),
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + path.extname(file.originalname));
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => cb(null, path.join(__dirname, "..", "uploads")),
+//   filename: (req, file, cb) => {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     cb(null, uniqueSuffix + path.extname(file.originalname));
+//   },
+// });
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "buzzink_post_images",
+    allowedFormats: ["jpg", "png", "jpeg", "gif", "webp", "avif"],
+    transformation: [{ width: 800, height: 600, crop: "limit" }],
   },
 });
 
