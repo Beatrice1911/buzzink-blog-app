2d9d8393a899c01f5c1746c9e857fc795c51945e
Beatrice Okereke <beatriceokereke19@gmail.com>
2025-12-26 11:53:05 +0100

    commit

--- backend/cloudinary.js
+++ backend/cloudinary.js
@@ -1,10 +1,9 @@
-import cloudinary from 'cloudinary';
-import { v2 as cloudinaryV2 } from 'cloudinary';
+const cloudinary = require('cloudinary').v2;
 
-cloudinaryV2.config({
+cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export default cloudinaryV2;
+module.exports = cloudinary;
