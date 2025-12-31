8e766409aada1d98318c5a7f2565b9584ea71775
Beatrice Okereke <beatriceokereke19@gmail.com>
2026-01-01 00:05:23 +0100

    update

--- backend/config/sendEmail.js
+++ backend/config/sendEmail.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, html }) => {
     await resend.emails.send({
-    from: "BuzzInk <no-reply@buzzink.com>",
+    from: "BuzzInk <onboarding@resend.dev>",
     to,
     subject,
     html,
