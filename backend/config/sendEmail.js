b3e3dec010a7003df5aa416a4847236b0d4ea6f7
Beatrice Okereke <beatriceokereke19@gmail.com>
2026-01-01 00:24:55 +0100

    update

--- backend/config/sendEmail.js
+++ backend/config/sendEmail.js
@@ -5,9 +5,15 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async ({ to, subject, html }) => {
     await resend.emails.send({
     from: "BuzzInk <onboarding@resend.dev>",
-    to,
-    subject,
-    html,
+    to: email,
+    subject: "Password Reset Request",
+    html: `
+    <h2>Password Reset</h2>
+    <p>You requested to reset your password.</p>
+    <p>Click the link below to reset it:</p>
+    <a href="${resetUrl}">${resetUrl}</a>
+    <p>This link will expire in 30 minutes.</p>
+  `
   });
 };
 
