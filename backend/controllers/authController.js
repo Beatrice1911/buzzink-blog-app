76a2be6066ebcbd98b991255d5f2f67284498ffc
Beatrice Okereke <beatriceokereke19@gmail.com>
2026-01-01 01:54:00 +0100

    commit

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -171,14 +171,14 @@ exports.forgotPassword = async (req, res) => {
 
     const resetToken = crypto.randomBytes(32).toString('hex');
     user.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
-    user.resetPasswordExpire = Date.now() + 60 * 60 * 1000; // 1 hour
+    user.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
 
     await user.save();
 
     const resetUrl = `${process.env.CLIENT_URL}/reset-password.html?token=${resetToken}`;
 
     await sendEmail({
-      to: user.email,
+      to: email,
       subject: "Password Reset Request",
       html: `
         <h2>Password Reset</h2>
