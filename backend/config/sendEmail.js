f1bca0c09eb9f7f1bd3dc934ff9599e0562aed22
Beatrice Okereke <beatriceokereke19@gmail.com>
2026-01-01 02:07:22 +0100

    update

--- backend/config/sendEmail.js
+++ backend/config/sendEmail.js
@@ -1,14 +1,38 @@
-const { Resend } = require("resend");
+// const { Resend } = require("resend");
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// const sendEmail = async ({ to, subject, html }) => {
+//   try {
+//     await resend.emails.send({
+//     from: "BuzzInk <onboarding@resend.dev>",
+//     to,
+//     subject,
+//     html
+//     });
+//   } catch (error) {
+//     console.error("Error sending email:", error);
+//     throw new Error("Failed to send email");
+//   }
+// };
+
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 const sendEmail = async ({ to, subject, html }) => {
   try {
-    await resend.emails.send({
-    from: "BuzzInk <onboarding@resend.dev>",
-    to,
-    subject,
-    html
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      html,
     });
   } catch (error) {
     console.error("Error sending email:", error);
