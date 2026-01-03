b94358706e3db2b9acd09a9faf5e0791d6a53674
Beatrice Okereke <beatriceokereke19@gmail.com>
2026-01-03 21:32:20 +0100

    update

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { verifyToken, isAdmin } = require("../middleware/auth");
-const { getAllUsers, deleteUser, getAllPosts, deleteAnyPost, getAllComments, deleteComment } = require('../controllers/adminController');
+const { getAllUsers, deleteUser, getAllPosts, deleteAnyPost, getAllComments, deleteComment, getAdminStats } = require('../controllers/adminController');
 
 router.get("/users", verifyToken, isAdmin, getAllUsers);
 router.delete('/users/:id', verifyToken, isAdmin, deleteUser);
@@ -9,5 +9,6 @@ router.get('/posts', verifyToken, isAdmin, getAllPosts);
 router.delete("/posts/:id", verifyToken, isAdmin, deleteAnyPost);
 router.get('/comments', verifyToken, isAdmin, getAllComments);
 router.delete('/comments/:id', verifyToken, isAdmin, deleteComment);
+router.get('/stats', verifyToken, isAdmin, getAdminStats);
 
 module.exports = router;
