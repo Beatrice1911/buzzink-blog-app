5c15af92e078677fb666396916d85305d675885c
Beatrice Okereke <beatriceokereke@gmail.com>
2026-01-29 22:19:31 +0100

    Serve dist folder in server.js

--- backend/server.js
+++ backend/server.js
@@ -63,9 +63,9 @@ app.use((err, req, res, next) => {
     .json({ message: err.message || "Server error" });
 });
 
-app.use(express.static(path.join(__dirname, "../frontend")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 app.get(/^\/(?!api).*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
 
 app.use(
