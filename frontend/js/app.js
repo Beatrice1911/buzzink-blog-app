1440e9b74963d0b00d2868149b534b96ddb10f7e
Beatrice Okereke <beatriceokereke@gmail.com>
2026-01-31 16:00:33 +0100

    Add pageshow event listener to reload page on back navigation

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -66,3 +66,9 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   initPostForm();
 });
+
+window.addEventListener("pageshow", (event) => {
+  if (event.persisted) {
+    window.location.reload();
+  }
+});
